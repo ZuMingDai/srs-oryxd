461ac243835e810185e8713eba013c182b9679e6
ZuMingDai <dzm327@163.com>
2015-12-28 23:27:04 +0800

    modiry

--- core/utiliry.go
+++ core/utiliry.go
@@ -1,11 +1,11 @@
 package core
 
 import (
-	"runtime"
 	"log"
 	"os"
+	"runtime"
 )
 
 func ServerRun(c *Config, callback func() int) int {
-	LogerTrace.PrintIn("apply log tank", c.Log.tank)
+	LogerTrace.PrintIn("apply log tank", c.Log.Tank)
 }
