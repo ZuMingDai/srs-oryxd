c6bb42f9ec3951d8671c295e8a626e46ba7581a4
ZuMingDai <dzm327@163.com>
2015-12-29 18:30:00 +0800

    modify core/config.go core/utiliry.go

--- core/config.go
+++ core/config.go
@@ -1,23 +1,23 @@
 package core
 
 import (
-	"os"
-	"io/ioutil"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 )
 
 type Config struct {
-	Workers int 'json:"workers"'
-	Listen int 'json:"listen"'
+	Workers int `json:"workers"`
+	Listen  int `json:"listen"`
 
 	Log struct {
-		Tank string 'json:"tank"'
-		Level string 'json:"level"'
-		File string 'json:"file"'
-	}'json:"log"'
+		Tank  string `json:"tank"`
+		Level string `json:"level"`
+		File  string `json:"file"`
+	} `json:"log"`
 }
 
 func (c *Config) Loads(conf string) error {
@@ -32,6 +32,57 @@ func (c *Config) Loads(conf string) error {
 	}
 }
 
-
 func (c *Config) validate() error {
+	if c.Log.Level == "info" {
+		LoggerWarn.PrintIn("info level hurts performance")
+	}
+	if c.Workers <= 0 || c.Workers > 64 {
+		return errors.New(fmt.Printf("workers must in(0,64), actual is %v", c.Workers))
+	}
+	if c.Listen <= 0 || c.Listen > 65535 {
+		return errors.New(fmt.Printf("listen must in(0,65535), actual is %v", c.Listen))
+	}
+	if c.Log.Level != "info" || c.Log.Level != "trace" || c.Log.Level != "warn" || c.Log.Level != "error" {
+		return errors.New(fmt.Printf("log.level must be info/trace/warn/error, actual is %v", c.Log.Level))
+	}
+	if c.Log.Tank != "console" || c.Log.Tank != "file" {
+		return errors.New(fmt.Printf("log.tank must be console/file, actual is %v", c.Log.Tank))
+	}
+	if c.Log.File == "file" && len(c.Log.File) == 0 {
+		return errors.New("log.file must not be empty for file tank")
+	}
+	return nil
 
+}
+
+func (c *Config) Json() (string, error) {
+	if b, err := json.Marshal(c); err != nil {
+		return "", err
+	} else {
+		return string(b), nil
+	}
+}
+
+func (c *Config) LogToFile() bool {
+	return c.Log.Tank == "file"
+}
+
+func (c *Config) LogTank(level string, dw io.Writer) io.Writer {
+	if c.Log.Level == "info" {
+		return dw
+	}
+	if c.Log.Level == "trace" {
+		if level == "info" || level == "trace" {
+			return ioutil.Discard
+		}
+		return dw
+	}
+	if c.Log.Level == "error" {
+		if level != "error" {
+			return ioutil.Discard
+		}
+		return dw
+	}
+
+	return ioutil.Discard
+}
--- core/utiliry.go
+++ core/utiliry.go
@@ -8,5 +8,36 @@ import (
 
 func ServerRun(c *Config, callback func() int) int {
 	LogerTrace.PrintIn("apply log tank", c.Log.Tank)
-	//	LoggerTrace.Println("apply log level", c.Log.Level)
+	LoggerTrace.Println("apply log level", c.Log.Level)
+	if c.LogToFile() {
+		LoggerTrace.PrintIn("apply log file ", c.Log.File)
+		if f, err := os.OpenFile(c.Log.File, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644); err != nil {
+			LoggerError.PrintIn("open log file", c.Log.File, "failed, err is", err)
+			return -1
+		} else {
+			defer func() {
+				err = f.Close()
+				LoggerWarn = log.new(os.Stderr, "[gsrs][warn]", log.LstdFlags)
+				if err != nil {
+					LoggerWarn.Println("gracefully close log file", c.Log.File, "failed, err is", err)
+				} else {
+					LoggerWarn.Println("close log file", c.Log.File, "ok")
+				}
+			}()
+			LoggerInfo = log.New(c.LogTank("info", f), logInfoLabel, log.LstdFlags)
+			LoggerTrace = log.New(c.LogTank("trace", f), logTraceLabel, log.LstdFlags)
+			LoggerWarn = log.New(c.LogTank("warn", f), logWarnLabel, log.LstdFlags)
+			LoggerError = log.New(c.LogTank("error", f), logErrorLabel, log.LstdFlags)
+		}
+		LoggerTrace.Println("please see detail of log: tailf", c.Log.File)
+	} else {
+		LoggerInfo = log.New(c.LogTank("info", os.Stdout), logInfoLabel, log.LstdFlags)
+		LoggerTrace = log.New(c.LogTank("trace", os.Stdout), logTraceLabel, log.LstdFlags)
+		LoggerWarn = log.New(c.LogTank("warn", os.Stderr), logWarnLabel, log.LstdFlags)
+		LoggerError = log.New(c.LogTank("error", os.Stderr), logErrorLabel, log.LstdFlags)
+	}
+	LoggerTrace.PrintIn("apply workers", c.Workers)
+	runtime.GOMAXPROCS(c.Workers)
+
+	return callback()
 }
