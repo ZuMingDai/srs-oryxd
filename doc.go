c8ab4b1411bec4e0bf03aa0f792cd2be55586918
ZuMingDai <dzm327@163.com>
2016-01-13 10:57:08 +0800

    add doc for srs

---
+++ doc.go
@@ -0,0 +1,9 @@
+/*
+Please use the follwing components:
+* srs: the srs server.
+Fox example:"
+	go build -o objs/srs github.com/ZuMingDai/srs-oryxd/srs
+	&& ./obje/srs -c conf/console.json
+*/
+
+package main
---
+++ srs/doc.go
@@ -0,0 +1,5 @@
+/*
+SRS is industrial-strength live streaming cluster, for the best conceptual integrity and the simplest implementation.
+*/
+
+package main
